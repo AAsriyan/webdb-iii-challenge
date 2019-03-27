49fbbaf84f55e3b5dd96fdc9fe9295750adb7ac2
AAsriyan <aasriyan@protonmail.com>
2019-03-27 15:32:00 -0400

    Set up DB with knex and GET endpoint working

---
+++ controllers/student-router.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const knex = require("knex");
+const db = require("../config/knexConfig.js");
+
+router.get("/", async (req, res) => {
+  try {
+    const result = await db("students");
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
